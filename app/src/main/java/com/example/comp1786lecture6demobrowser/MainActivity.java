f8d69f4bd5d3a07ef98e799cb903c31cfc2790a9
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-28 15:50:51 +0700

    Intercepting the Back Button

--- app/src/main/java/com/example/comp1786lecture6demobrowser/MainActivity.java
+++ app/src/main/java/com/example/comp1786lecture6demobrowser/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.comp1786lecture6demobrowser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -32,5 +33,16 @@ public class MainActivity extends AppCompatActivity {
             view.loadUrl(request.getUrl().toString());
             return true;
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if((keyCode == KeyEvent.KEYCODE_BACK)
+            && browser.canGoBack()
+        ){
+            browser.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
